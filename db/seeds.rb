35cd62ce726cfc273679a843766908c890aea4f2
加藤義隆 <yoshitakakato@katouyoshitakanoMacBook-Air.local>
2021-12-29 15:01:23 +0900

    first-commit

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Player.create(name: 'Ogino Takashi', age: 36)
+Player.create(name: 'Fujiwara Kyota', age: 21)
+Player.create(name: 'Nakamura Shogo', age: 29)
